5349e9d6b1c1455b5561984be97bbad1ae70c170
yashGzz01 <yashgoyal2910786@gmail.com>
2022-09-17 10:26:27 +0530

    commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import './sidebar.css'
+
+import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
+
+const SidebarItem = props => {
+
+    const active = props.active ? 'active' : ''
+
+    return (
+        <div className="sidebar__item">
+            <div className={`sidebar__item-inner ${active}`}>
+                <i className={props.icon}></i>
+                <span>
+                    {props.title}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+const Sidebar = props => {
+
+    const activeItem = sidebar_items.findIndex(item => item.route === props.location.pathname)
+
+    return (
+        <div className='sidebar'>
+            <div className="sidebar__logo">
+                <img src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=height:60/https://file-uploads.teachablecdn.com/ffe442a414904138b18e5b532bba9da1/167af06d542e43a999f07c44645dbc1d" srcSet="" alt="company logo" />
+                
+            </div>
+            {
+                sidebar_items.map((item, index) => (
+                    <Link to={item.route} key={index}>
+                        <SidebarItem
+                            title={item.display_name}
+                            icon={item.icon}
+                            active={index === activeItem}
+                        />
+                    </Link>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Sidebar
